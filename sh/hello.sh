62a8198a4bfbebe04498790dd97fcef1e00c9ffd
eformat <eformat@gmail.com>
2016-10-23 13:46:46 +1000

    added foo

--- sh/hello.sh
+++ sh/hello.sh
@@ -1,2 +1,3 @@
 #!/bin/sh
-while true; do echo -e "HTTP/1.1 200 OK\n\nHello ! Welcome to OpenShift 3" | nc -ll -p 8080; done
+name=${1:-World};
+while true; do echo -e "HTTP/1.1 200 OK\n\nHello ${name} ! Welcome to OpenShift 3" | nc -ll -p 8080; done
